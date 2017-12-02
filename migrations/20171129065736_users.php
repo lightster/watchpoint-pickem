190913b3a56aa9611eaa7c0273d0eb3d4c2afff9
Zachary Rankin <rankin.zachary@gmail.com>
2017-12-02 05:59:47 +0000

    Create users table

---
+++ migrations/20171129065736_users.php
@@ -0,0 +1,20 @@
+<?php
+
+if ($rollback === true) {
+    $app->db()->query("DROP TABLE users;");
+    return;
+}
+
+// create users table
+$sql = <<<SQL
+CREATE TABLE users (
+    user_id serial PRIMARY KEY,
+    bnet_account_id varchar NOT NULL,
+    bnet_tag varchar NOT NULL,
+    email varchar,
+    created_at timestamp with time zone DEFAULT now() NOT NULL,
+    updated_at timestamp with time zone DEFAULT now() NOT NULL
+);
+SQL;
+
+$app->db()->query($sql);
