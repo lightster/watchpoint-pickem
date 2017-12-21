98ac622b59f9476a0351514da5aeab978474e1be
Zachary Rankin <rankin.zachary@gmail.com>
2017-12-21 21:38:32 +0000

    Fix week end date to not overlap

--- migrations/20171215022901_matches_view.php
+++ migrations/20171215022901_matches_view.php
@@ -12,7 +12,7 @@ CREATE VIEW match_weeks AS
 SELECT
     match_id,
     date_trunc('week', game_time) :: date AS start_of_week,
-    (date_trunc('week', game_time) + '7 days') :: date AS end_of_week,
+    (date_trunc('week', game_time) + '6 days') :: date AS end_of_week,
     dense_rank() OVER (ORDER BY date_trunc('week', game_time)) AS week_number
 FROM matches
 SQL;
