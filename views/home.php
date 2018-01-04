5fc6cc106c0471ba69c4efbc99fc94aa30af54b0
Zachary Rankin <rankin.zachary@gmail.com>
2018-01-04 19:39:21 +0000

    Simplify homepage
    The homepage was just static and did not have any useful information on
    it. I have added some basic information and some screenshots to show the
    basic functionality.

--- views/home.php
+++ views/home.php
@@ -1,34 +1,38 @@
-<main role="main">
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>watchpoint.io</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <section class="jumbotron text-center">
-    <div class="container">
-      <h1 class="jumbotron-heading">Overwatch League Fantasy Pick'Em</h1>
-      <p class="lead text-muted">Come have some fun with us in our fantasy Overwatch league. Create your own pool or join one of ours and win some fun prizes.</p>
-      <p>
-        <a href="#" class="btn btn-primary">Sign up</a>
-      </p>
-    </div>
-  </section>
-
-  <div class="album text-muted">
-    <div class="container">
-
-      <div class="row">
-        <div class="card">
-          <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
-          <p class="card-text"><strong>Crazy Wild Pool Party</strong> Example featured pool.</p>
-        </div>
-        <div class="card">
-          <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
-          <p class="card-text"><strong>ASUS Sponsored Pool</strong> Weekly winners get a new ASUS monitor.</p>
-        </div>
-        <div class="card">
-          <img data-src="holder.js/100px280?theme=thumb" alt="Card image cap">
-          <p class="card-text"><strong>We Rock</strong> Special pool for us only.</p>
-        </div>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/home.css">
+  </head>
+  <body>
+    <div class="container p-5">
+      <div class="logo">
+        <img src="/img/logo.svg" width="300">
       </div>
-
+      <div class="row">
+        <div class="col">
+          <p class="mt-5">
+            <a href="/auth" class="btn btn-lg btn-primary">Sign in with
+              Battle.net</a>
+          </p>
+          <h2 class="mt-5">Overwatch League Fantasy Pick'Em</h2>
+          <p>
+            Compete against your friends by selecting the team that you think
+            will win each week in the Overwatch League.
+          </p>
+          <h3 class="mt-5">Top the Leaderboard</h3>
+          <p><img src="/img/watchpoint-leaderboard.png" class="img-fluid"></p>
+          <h3 class="mt-5">Make Your Picks</h3>
+          <p><img src="/img/watchpoint-picks.png" class="img-fluid"></p>
+          <h3 class="mt-5">Pick the Winner</h3>
+          <p><img src="/img/watchpoint-picks-winners.png" class="img-fluid"></p>
+        </div>
     </div>
-  </div>
-
-</main>
+  </body>
+</html>
